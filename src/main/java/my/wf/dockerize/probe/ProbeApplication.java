137c1539e87a6d5076e9db7f82b3665f407f7d22
SergWF <sergii.bilenogov@de.bosch.com>
2018-01-28 13:00:20 +0100

    add Dockerfiles and related files

---
+++ src/main/java/my/wf/dockerize/probe/ProbeApplication.java
@@ -0,0 +1,12 @@
+package my.wf.dockerize.probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbeApplication.class, args);
+	}
+}
